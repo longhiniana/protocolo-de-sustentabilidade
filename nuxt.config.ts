1c86aa8df91c39b2cfce8ff9bfa2d0210d08ec88
André Vandal <1340508+andrevandal@users.noreply.github.com>
2022-10-16 21:22:51 -0300

    fix: transpile resize-detector

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   build: {
-    transpile: [/echarts/]
+    transpile: [/echarts/, /resize-detector/]
   },
   modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt'],
 })
